d9a3d88711e2ec30f0f99ca9cfe2a7c95701855b
Brandon <b.cacsire8@gmail.com>
2023-05-21 05:07:26 -0400

    express server on PORT 8000 hosting react app

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 // import express
 const express = require("express")
 
+// import path module
+const path = require('path')
+
 // define a PORT for our server to run
 const PORT = 8000
 
 // initialize express application
 const app = express()
 
+// host react app as static files
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
 // define some routes
 app.get('/', (req, res) => {
- res.send("Hey don't forget we're testing route /api")
+ res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 app.get('/api', (req, res) => {
  // send message back to client
